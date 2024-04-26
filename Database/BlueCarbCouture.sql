0a92a79d8cfbd703fba4f37953d78397fcf000c1
VuScriptMasterForge <vunhph33506@fpt.edu.vn>
2024-04-26 20:16:37 +0700

    Update database

--- Database/BlueCarbCouture.sql
+++ Database/BlueCarbCouture.sql
@@ -2,131 +2,134 @@ CREATE DATABASE [BlueCrabCouture]
 GO
 USE [BlueCrabCouture]
 GO
-CREATE TABLE [Role](
-	[id] INT IDENTITY PRIMARY KEY,
+CREATE TABLE [UserRole](
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
 	[role_name] NVARCHAR(20) UNIQUE NOT NULL,
-	[create_date] DATE DEFAULT GETDATE()
+	[create_date] DATE DEFAULT GETDATE() NULL
 )
 GO
-CREATE TABLE [User](
-	[id] INT IDENTITY PRIMARY KEY,
+CREATE TABLE [UserAccount](
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
 	[phone_number] NVARCHAR(13) UNIQUE NOT NULL,
+	[email] NVARCHAR(100) UNIQUE NOT NULL,
 	[password] NVARCHAR(50) NOT NULL,
-	[id_role] INT FOREIGN KEY REFERENCES[Role](id),
-	[is_locked] BIT DEFAULT 0,
-	[create_date] DATE DEFAULT GETDATE()
+	[id_user_role] INT FOREIGN KEY REFERENCES[UserRole](id) NOT NULL,
+	[is_locked] BIT DEFAULT 0 NOT NULL,
+	[create_date] DATE DEFAULT GETDATE() NULL
 )
 GO
-CREATE TABLE [Address](
+CREATE TABLE [UserAddress](
 	[id] INT IDENTITY PRIMARY KEY,
-	[city] NVARCHAR(50),
-	[district] NVARCHAR(50),
-	[specific_information] NVARCHAR(255)
+	[city] NVARCHAR(50) NOT NULL,
+	[district] NVARCHAR(50) NOT NULL,
+	[specific_information] NVARCHAR(255) NOT NULL
 )
 GO
-CREATE TABLE [User_detail](
-	[id] INT IDENTITY PRIMARY KEY,
-	[name] NVARCHAR(50),
-	[gender] BIT,
-	[date_of_birth] DATE,
-	[id_user] INT FOREIGN KEY REFERENCES[User](id),
-	[id_address] INT FOREIGN KEY REFERENCES[Address](id),
-	[last_edited] DATE DEFAULT GETDATE()
+CREATE TABLE [UserAccountDetail](
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[name] NVARCHAR(50) NULL,
+	[gender] BIT DEFAULT 0,
+	[date_of_birth] DATE NULL,
+	[avatar] NVARCHAR(255) NULL,
+	[id_user_account] INT FOREIGN KEY REFERENCES[UserAccount](id) UNIQUE NULL,
+	[id_user_address] INT FOREIGN KEY REFERENCES[UserAddress](id) UNIQUE NULL,
+	[last_edited] DATE DEFAULT GETDATE() NULL
 )
 GO
 CREATE TABLE [Category](
-	[id] INT IDENTITY PRIMARY KEY,
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
 	[url_logo] NVARCHAR(255),
-	[name] NVARCHAR(50),
+	[name] NVARCHAR(50) NOT NULL UNIQUE,
 	[create_date] DATE DEFAULT GETDATE()
 )
 GO
 CREATE TABLE [Brand](
-	[id] INT IDENTITY PRIMARY KEY,
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
 	[url_logo] NVARCHAR(255),
-	[name] NVARCHAR(50),
-	[create_date] DATE DEFAULT GETDATE()
+	[name] NVARCHAR(50) NOT NULL UNIQUE,
+	[create_date] DATE DEFAULT GETDATE() NULL
 )
 GO
 CREATE TABLE [Color](
-	[id] INT IDENTITY PRIMARY KEY,
-	[color_code] NVARCHAR(20),
-	[name] NVARCHAR(20),
-	[create_date] DATE DEFAULT GETDATE()
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[color_code] NVARCHAR(20) NOT NULL,
+	[name] NVARCHAR(20) UNIQUE NOT NULL,
+	[create_date] DATE DEFAULT GETDATE() NULL
 )
 GO
 CREATE TABLE [Size](
-	[id] INT IDENTITY PRIMARY KEY,
-	[name] NVARCHAR(20) UNIQUE,
-	[create_date] DATE DEFAULT GETDATE()
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[name] NVARCHAR(20) UNIQUE NOT NULL,
+	[create_date] DATE DEFAULT GETDATE() NULL
 )
 GO
 CREATE TABLE [Image](
-	[id] INT IDENTITY PRIMARY KEY,
-	[url] NVARCHAR(100),
-	[create_date] DATE DEFAULT GETDATE()
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[url] NVARCHAR(100) UNIQUE NOT NULL,
+	[create_date] DATE DEFAULT GETDATE() NULL
 )
 GO
 CREATE TABLE [Product](
-	[id] INT IDENTITY PRIMARY KEY,
-	[code] NVARCHAR(255),
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[code] NVARCHAR(255) NULL,
 	[name] NVARCHAR(255) NOT NULL,
-	[sold] INT DEFAULT 0,
-	[id_image] INT FOREIGN KEY REFERENCES[Image](id),
+	[sold] INT DEFAULT 0 NULL,
+	[id_image] INT FOREIGN KEY REFERENCES[Image](id) NULL,
 	[is_deleted] BIT DEFAULT 0,
-	[id_user] INT FOREIGN KEY REFERENCES[User](id),
-	[create_date] DATE DEFAULT GETDATE()
+	[id_user_account] INT FOREIGN KEY REFERENCES[UserAccount](id) NOT NULL,
+	[create_date] DATE DEFAULT GETDATE() NULL
 )
 GO
 CREATE TABLE [ProductDetail](
-	[id] INT IDENTITY PRIMARY KEY,
-	[id_category] INT FOREIGN KEY REFERENCES[Category](id),
-	[id_brand] INT FOREIGN KEY REFERENCES[Brand](id),
-	[id_color] INT FOREIGN KEY REFERENCES[Color](id),
-	[id_size] INT FOREIGN KEY REFERENCES[Size](id),
-	[id_product] INT FOREIGN KEY REFERENCES[Product](id),
-	[price] BIGINT NOT NULL,
-	[quantity] INT DEFAULT 0,
-	[create_date] DATE DEFAULT GETDATE()
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[id_category] INT FOREIGN KEY REFERENCES[Category](id) NOT NULL,
+	[id_brand] INT FOREIGN KEY REFERENCES[Brand](id) NOT NULL,
+	[id_color] INT FOREIGN KEY REFERENCES[Color](id) NOT NULL,
+	[id_size] INT FOREIGN KEY REFERENCES[Size](id) NOT NULL,
+	[id_product] INT FOREIGN KEY REFERENCES[Product](id) NOT NULL,
+	[price] DECIMAL(10, 2) CONSTRAINT check_price CHECK([price] > 10) NOT NULL,
+	[quantity] INT DEFAULT 0 NULL,
+	[create_date] DATE DEFAULT GETDATE() NOT NULL
 )
 GO
-CREATE TABLE [Cart](
-	[id] INT IDENTITY PRIMARY KEY,
-	[id_user] INT FOREIGN KEY REFERENCES[User](id),
-	[quantity_product] INT,
-	[total_money] BIGINT
+CREATE TABLE [ShoppingCart](
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[id_user] INT FOREIGN KEY REFERENCES[UserAccount](id) NOT NULL,
+	[quantity_product] INT DEFAULT 0,
+	[total_money] DECIMAL(10, 2) DEFAULT 0
 )
 GO
-CREATE TABLE [CartDetail](
-	[id] INT IDENTITY PRIMARY KEY,
-	[price] BIGINT,
-	[quantity] INT,
-	[id_product_detail] INT FOREIGN KEY REFERENCES[ProductDetail](id),
-	[add_date] DATE DEFAULT GETDATE(),
-	[last_edited] DATE DEFAULT GETDATE()
+CREATE TABLE [ShoppingCartDetail](
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[price] DECIMAL(10, 2) NULL,
+	[quantity] INT CONSTRAINT check_quantity CHECK ([quantity] > 1),
+	[id_shopping_cart] INT FOREIGN KEY REFERENCES[ShoppingCart](id) NOT NULL,
+	[id_product_detail] INT FOREIGN KEY REFERENCES[ProductDetail](id) NOT NULL,
+	[add_date] DATE DEFAULT GETDATE() NULL,
+	[last_edited] DATE DEFAULT GETDATE() NULL
 )
 GO
-CREATE TABLE [StatusBill](
-	[id] INT IDENTITY PRIMARY KEY,
+CREATE TABLE [InvoiceStatus](
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
 	[name] NVARCHAR(50) UNIQUE NOT NULL
 )
 GO
-CREATE TABLE [Bill](
-	[id] INT IDENTITY PRIMARY KEY,
-	[code_bill] NVARCHAR(50) UNIQUE NOT NULL,
-	[id_user] INT FOREIGN KEY REFERENCES[User](id),
-	[id_status] INT FOREIGN KEY REFERENCES[StatusBill](id),
-	[total_money] BIGINT,
-	[quantity_product] INT,
-	[create_date] DATE DEFAULT GETDATE()
+CREATE TABLE [Orders](
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[code_order] NVARCHAR(50) UNIQUE NOT NULL,
+	[id_user_account] INT FOREIGN KEY REFERENCES[UserAccount](id) NOT NULL,
+	[id_status_invoice] INT FOREIGN KEY REFERENCES[InvoiceStatus](id) NOT NULL,
+	[total_money] DECIMAL(10, 2) DEFAULT 0,
+	[quantity_product] INT DEFAULT 0,
+	[create_date] DATE DEFAULT GETDATE() NULL
 )
 GO
-CREATE TABLE [BillDetail](
-	[id] INT IDENTITY PRIMARY KEY,
-	[id_bill] INT FOREIGN KEY REFERENCES[Bill](id),
-	[price] BIGINT,
-	[quantity] INT,
-	[id_product_detail] INT FOREIGN KEY REFERENCES[ProductDetail](id),
-	[add_date] DATE DEFAULT GETDATE(),
-	[last_edited] DATE DEFAULT GETDATE()
+CREATE TABLE [OrderItems](
+	[id] INT IDENTITY PRIMARY KEY NOT NULL,
+	[id_order] INT FOREIGN KEY REFERENCES[Orders](id) NOT NULL,
+	[price] DECIMAL(10, 2) NOT NULL,
+	[quantity] INT CONSTRAINT check_quantity_item CHECK ([quantity] > 1),
+	[id_product_detail] INT FOREIGN KEY REFERENCES[ProductDetail](id)  NOT NULL,
+	[add_date] DATE DEFAULT GETDATE() NULL,
+	[last_edited] DATE DEFAULT GETDATE() NULL
 )
